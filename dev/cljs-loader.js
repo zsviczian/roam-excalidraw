5ea3c1ab24589d026918e6968eb65ee11e49295b
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 19:04:58 +0100

    moved cljs loader into object so I can delete after loading.

--- dev/cljs-loader.js
+++ dev/cljs-loader.js
@@ -1,106 +1,117 @@
-const sketchingUID  = 'sketching';
-const excalDATAUID = 'ExcalDATA';
-const pageTitle = 'roam/excalidraw';
-const mainComponentParent = "Main Component";
-const dataComponentParent = "Data Block Component";
+window.ExcalidrawLoader = {
+  sketchingUID : 'sketching',
+  excalDATAUID : 'ExcalDATA',
+  pageTitle : 'roam/excalidraw',
+  mainComponentParent : "Main Component",
+  dataComponentParent : "Data Block Component",
 
-function getClojureNS(blockUID) {
-  const q = `[:find ?s . :where [?e :block/uid "${blockUID}"][?e :block/string ?s]]`;
-  const renderString = window.roamAlphaAPI.q(q);
-  if(renderString != null) { 
-    ptrn = /\(ns (.*)\s/g;
-    const res = ptrn.exec(renderString);
-    if(res == null) return '';
-    return res[1];
+  getClojureNS(blockUID) {
+    const q = `[:find ?s . :where [?e :block/uid "${blockUID}"][?e :block/string ?s]]`;
+    const renderString = window.roamAlphaAPI.q(q);
+    if(renderString != null) { 
+      ptrn = /\(ns (.*)\s/g;
+      const res = ptrn.exec(renderString);
+      if(res == null) return '';
+      return res[1];
+    }
+    return '';
+  },
+
+  updateCodeBlock(blockUID, sourceCode) {
+    window.roamAlphaAPI.updateBlock({"block": 
+                                    {"string": sourceCode,
+                                      "uid": blockUID}});
+  },
+
+  createBlockWithUID(parentUID, order, blockString, blockUID) {
+    window.roamAlphaAPI.createBlock({"location":
+                                    {"parent-uid": parentUID, 
+                                    "order": order}, 
+                                      "block": {"string": blockString,
+                                                "uid": blockUID}});  
+  },
+
+  createBlock(parentUID, order, blockString) {
+    const blockUID = window.roamAlphaAPI.util.generateUID();
+    this.createBlockWithUID (parentUID, order, blockString, blockUID);
+    return blockUID;
+  },
+
+  getBlockUIDByStringANDOrder (pageUID, order, blockString) {
+    const q = `[:find ?uid . :where [?p :block/uid "${pageUID}"]
+                            [?p :block/children ?b]
+                            [?b :block/order ${order}]
+                            [?b :block/string ?s]
+                            [(= ?s "${blockString}")]
+                            [?b :block/uid ?uid]]`;
+    return window.roamAlphaAPI.q(q);
+  },
+
+  getORcreateBlockBYString (pageUID, order, blockString) {
+    let uid = this.getBlockUIDByStringANDOrder (pageUID, order, blockString);
+    if (uid == null)
+      uid = createBlock(pageUID,order, blockString);
+    return uid;
+  },
+
+  blockExists(blockUID) {
+    const q = `[:find ?e . :where [?e :block/uid "${blockUID}"]]`;
+    const res = window.roamAlphaAPI.q(q);  
+    return (res!=null);
+  }, 
+
+  createBlockIfNotExists (parentUID, blockUID, blockString) {
+    if(this.blockExists(blockUID))
+      this.updateCodeBlock(blockUID,blockString);
+    else
+      this.createBlockWithUID (parentUID,0,blockString,blockUID);
+  },
+
+  buildPage() {
+    //check if page exists, if not, create it
+    let q= `[:find ?uid . :where [?e :node/title "${this.pageTitle}"][?e :block/uid ?uid]]`;
+    let pageUID = window.roamAlphaAPI.q(q);
+    if(pageUID == null) {
+      pageUID = window.roamAlphaAPI.util.generateUID();
+      window.roamAlphaAPI.createPage({"page": 
+                                            {"title": this.pageTitle, 
+                                            "uid": pageUID}});
+    }
+    
+    function isParent(blockUID, parentUID) {
+      const q = `[:find ?uid . :where [?b :block/uid "${blockUID}"][?p :block/children ?b][?p :block/uid ?uid]]`;
+      const uid = window.roamAlphaAPI.q(q);
+      return (uid == parentUID);
+    }
+
+    const mainComponentParentUID = this.getORcreateBlockBYString (pageUID,0,this.mainComponentParent);
+    const dataComponentParentUID = this.getORcreateBlockBYString (pageUID,1,this.dataComponentParent);
+    this.createBlockIfNotExists (mainComponentParentUID, this.sketchingUID, '');
+    this.createBlockIfNotExists (dataComponentParentUID, this.excalDATAUID, '');
+
+    if(!isParent(this.sketchingUID,this.mainComponentParent))
+      window.roamAlphaAPI.moveBlock({"location":
+                                      {"parent-uid": mainComponentParentUID, 
+                                    "order": 0}, 
+                                      "block": {"uid": this.sketchingUID}});
+    if(!isParent(this.excalDATAUID,this.dataComponentParent))                                    
+      window.roamAlphaAPI.moveBlock({"location":
+                                      {"parent-uid": dataComponentParentUID, 
+                                    "order": 0}, 
+                                      "block": {"uid": this.excalDATAUID}});  
   }
-  return '';
 }
 
-function updateCodeBlock(blockUID, sourceCode) {
-  window.roamAlphaAPI.updateBlock({"block": 
-                                   {"string": sourceCode,
-                                    "uid": blockUID}});
-}
-
-function createBlockWithUID(parentUID, order, blockString, blockUID) {
-  window.roamAlphaAPI.createBlock({"location":
-                                   {"parent-uid": parentUID, 
-	                                "order": order}, 
-                                    "block": {"string": blockString,
-                                              "uid": blockUID}});  
-}
-
-function createBlock(parentUID, order, blockString) {
-  const blockUID = window.roamAlphaAPI.util.generateUID();
-  createBlockWithUID (parentUID, order, blockString, blockUID);
-  return blockUID;
-}
-
-function getBlockUIDByStringANDOrder (pageUID, order, blockString) {
-  const q = `[:find ?uid . :where [?p :block/uid "${pageUID}"]
-                           [?p :block/children ?b]
-                           [?b :block/order ${order}]
-                           [?b :block/string ?s]
-                           [(= ?s "${blockString}")]
-                           [?b :block/uid ?uid]]`;
-  return window.roamAlphaAPI.q(q);
-}
-
-function getORcreateBlockBYString (pageUID, order, blockString) {
-  let uid = getBlockUIDByStringANDOrder (pageUID, order, blockString);
-  if (uid == null)
-    uid = createBlock(pageUID,order, blockString);
-  return uid;
-}
-
-function blockExists(blockUID) {
-  const q = `[:find ?e . :where [?e :block/uid "${blockUID}"]]`;
-  const res = window.roamAlphaAPI.q(q);  
-  return (res!=null);
-} 
-
-function createBlockIfNotExists (parentUID, blockUID, blockString) {
-  if(blockExists(blockUID))
-    updateCodeBlock(blockUID,blockString);
-  else
-    createBlockWithUID (parentUID,0,blockString,blockUID);
-}
-
-function buildPage() {
-  //check if page exists, if not, create it
-  let q= `[:find ?uid . :where [?e :node/title "${pageTitle}"][?e :block/uid ?uid]]`;
-  let pageUID = window.roamAlphaAPI.q(q);
-  if(pageUID == null) {
-    pageUID = window.roamAlphaAPI.util.generateUID();
-    window.roamAlphaAPI.createPage({"page": 
-                                          {"title": pageTitle, 
-                                           "uid": pageUID}});
-  }
-  
-  const mainComponentParentUID = getORcreateBlockBYString (pageUID,0,mainComponentParent);
-  const dataComponentParentUID = getORcreateBlockBYString (pageUID,1,dataComponentParent);
-  createBlockIfNotExists (mainComponentParentUID, sketchingUID, '');
-  createBlockIfNotExists (dataComponentParentUID, excalDATAUID, '');
-  window.roamAlphaAPI.moveBlock({"location":
-                                  {"parent-uid": mainComponentParentUID, 
-	                               "order": 0}, 
-                                   "block": {"uid": sketchingUID}});
-  window.roamAlphaAPI.moveBlock({"location":
-                                  {"parent-uid": dataComponentParentUID, 
-	                               "order": 0}, 
-                                   "block": {"uid": excalDATAUID}});  
-}
-
-if (getClojureNS(sketchingUID) != ExcalidrawConfig.cljCodeVersion) {
-  buildPage();
+if (ExcalidrawLoader.getClojureNS(ExcalidrawLoader.sketchingUID) != ExcalidrawConfig.cljCodeVersion) {
+  ExcalidrawLoader.buildPage();
   const tripple_accent = String.fromCharCode(96,96,96);
-  updateCodeBlock(sketchingUID,tripple_accent + 
+  ExcalidrawLoader.updateCodeBlock(ExcalidrawLoader.sketchingUID,tripple_accent + 
                   'clojure\n' + 
                   ExcalidrawConfig.mainComponent +
                   tripple_accent);
   ExcalidrawConfig.mainComponent = null;
 
-  updateCodeBlock(excalDATAUID,tripple_accent + 
+  ExcalidrawLoader.updateCodeBlock(ExcalidrawLoader.excalDATAUID,tripple_accent + 
                   'clojure\n' + 
                   ExcalidrawConfig.dataComponent +
                   tripple_accent);
@@ -108,3 +119,4 @@ if (getClojureNS(sketchingUID) != ExcalidrawConfig.cljCodeVersion) {
 }
 
 delete ExcalidrawConfig.cljCodeVersion;
+delete ExcalidrawLoader;

57da2e604a44d00b50f909661827e607457a5de3
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 17:56:31 +0100

    .

--- get.php
+++ get.php
@@ -5,11 +5,11 @@
   header('content-type: application/javascript');
   $response = 'ExcalidrawConfig.mainComponent = `';
   $code = file_get_contents($path.'main-component.cljs');
-  $response = $response.$code.'`;\n';
+  $response = $response.$code.'`; ';
 
   $response = $response.'ExcalidrawConfig.dataComponent = `';
   $code = file_get_contents($path.'data-component.cljs');
-  $response = $response.$code.'`;\n';
+  $response = $response.$code.'`; ';
   $code = file_get_contents($path.'cljs-loader.js');
 
   echo $response.$code;

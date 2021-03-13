8486e46e20e1c97dac6be5133966b1cdd7fc4f67
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 10:12:27 +0100

    test

--- local-loader.js
+++ local-loader.js
@@ -1,14 +1,15 @@
 window['ExcalidrawConfig'] = {
-  rootPath: 'http://localhost:7010/roam-excalidraw-plugin/',
+  host : 'https://8236d001a819.eu.ngrok.io'
+  rootPath: '/roam-excalidraw-plugin/',
   cljCodeVersion: 'excalidraw.app.alpha.x',
   DEBUG: true,
   libs: [
-    ExcalidrawConfig.rootPath+'cljs-loader.js',
-    ExcalidrawConfig.rootPath+'main.js',
-    ExcalidrawConfig.rootPath+'lib/excalidraw-utils.min.js', // https://unpkg.com/@excalidraw/utils@0.1.0-temp/dist
+    this.rootPath+'cljs-loader.js',
+    this.rootPath+'main.js',
+    this.rootPath+'lib/excalidraw-utils.min.js', // https://unpkg.com/@excalidraw/utils@0.1.0-temp/dist
     'https://unpkg.com/react@17/umd/react.production.min.js',
     'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
-    ExcalidrawConfig.rootPath+'lib/excalidraw.min.js'
+    this.rootPath+'lib/excalidraw.min.js'
   ], // https://unpkg.com/@excalidraw/excalidraw@0.4.3/dist
 }
 

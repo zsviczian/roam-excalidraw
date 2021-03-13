adf9b53ac468d774f71a36fe5fd251792f9eda9f
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 17:30:34 +0100

    .

--- dev/local-loader.js
+++ dev/local-loader.js
@@ -6,10 +6,10 @@ ExcalidrawConfig.DEBUG = true;
 ExcalidrawConfig.libs = [
   ExcalidrawConfig.rootPath + 'get.php?f=main-component.cljs&c='+ExcalidrawConfig.channel,
   ExcalidrawConfig.rootPath + 'get.php?f=data-component.cljs&c='+ExcalidrawConfig.channel,
-  'https://unpkg.com/@excalidraw/utils@0.1.0-temp/dist/excalidraw-utils.min.js',
-  'https://unpkg.com/@excalidraw/excalidraw@0.4.3/dist/excalidraw.min.js',
   'https://unpkg.com/react@17/umd/react.production.min.js',
   'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+  'https://unpkg.com/@excalidraw/excalidraw@0.4.3/dist/excalidraw.min.js',
+  'https://unpkg.com/@excalidraw/utils@0.1.0-temp/dist/excalidraw-utils.min.js',
   ExcalidrawConfig.rootPath+ExcalidrawConfig.channel+'/cljs-loader.js',
   ExcalidrawConfig.rootPath+ExcalidrawConfig.channel+'/main.js',
 ]; 

11e9b516f77bfe07120682e3dc607556d2908641
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 19:47:51 +0100

    alpha release

---
+++ alpha/cljs-loader.js
@@ -0,0 +1,107 @@
+window.ExcalidrawLoader = {
+  sketchingUID : ExcalidrawConfig.sketchingUID,
+  excalDATAUID : ExcalidrawConfig.excalDATAUID,
+  pageTitle : 'roam/excalidraw',
+  mainComponentParent : "Main Component",
+  dataComponentParent : "Data Block Component",
+
+  updateCodeBlock(blockUID, sourceCode) {
+    window.roamAlphaAPI.updateBlock({"block": 
+                                    {"string": sourceCode,
+                                      "uid": blockUID}});
+  },
+
+  createBlockWithUID(parentUID, order, blockString, blockUID) {
+    window.roamAlphaAPI.createBlock({"location":
+                                    {"parent-uid": parentUID, 
+                                    "order": order}, 
+                                      "block": {"string": blockString,
+                                                "uid": blockUID}});  
+  },
+
+  createBlock(parentUID, order, blockString) {
+    const blockUID = window.roamAlphaAPI.util.generateUID();
+    this.createBlockWithUID (parentUID, order, blockString, blockUID);
+    return blockUID;
+  },
+
+  getBlockUIDByStringANDOrder (pageUID, order, blockString) {
+    const q = `[:find ?uid . :where [?p :block/uid "${pageUID}"]
+                            [?p :block/children ?b]
+                            [?b :block/order ${order}]
+                            [?b :block/string ?s]
+                            [(= ?s "${blockString}")]
+                            [?b :block/uid ?uid]]`;
+    return window.roamAlphaAPI.q(q);
+  },
+
+  getORcreateBlockBYString (pageUID, order, blockString) {
+    let uid = this.getBlockUIDByStringANDOrder (pageUID, order, blockString);
+    if (uid == null)
+      uid = createBlock(pageUID,order, blockString);
+    return uid;
+  },
+
+  blockExists(blockUID) {
+    const q = `[:find ?e . :where [?e :block/uid "${blockUID}"]]`;
+    const res = window.roamAlphaAPI.q(q);  
+    return (res!=null);
+  }, 
+
+  createBlockIfNotExists (parentUID, blockUID, blockString) {
+    if(this.blockExists(blockUID))
+      this.updateCodeBlock(blockUID,blockString);
+    else
+      this.createBlockWithUID (parentUID,0,blockString,blockUID);
+  },
+
+  buildPage() {
+    //check if page exists, if not, create it
+    let q= `[:find ?uid . :where [?e :node/title "${this.pageTitle}"][?e :block/uid ?uid]]`;
+    let pageUID = window.roamAlphaAPI.q(q);
+    if(pageUID == null) {
+      pageUID = window.roamAlphaAPI.util.generateUID();
+      window.roamAlphaAPI.createPage({"page": 
+                                            {"title": this.pageTitle, 
+                                            "uid": pageUID}});
+    }
+    
+    function isParent(blockUID, parentUID) {
+      const q = `[:find ?uid . :where [?b :block/uid "${blockUID}"][?p :block/children ?b][?p :block/uid ?uid]]`;
+      const uid = window.roamAlphaAPI.q(q);
+      return (uid == parentUID);
+    }
+
+    const mainComponentParentUID = this.getORcreateBlockBYString (pageUID,0,this.mainComponentParent);
+    const dataComponentParentUID = this.getORcreateBlockBYString (pageUID,1,this.dataComponentParent);
+    this.createBlockIfNotExists (mainComponentParentUID, this.sketchingUID, '');
+    this.createBlockIfNotExists (dataComponentParentUID, this.excalDATAUID, '');
+
+    if(!isParent(this.sketchingUID,this.mainComponentParent))
+      window.roamAlphaAPI.moveBlock({"location":
+                                      {"parent-uid": mainComponentParentUID, 
+                                    "order": 0}, 
+                                      "block": {"uid": this.sketchingUID}});
+    if(!isParent(this.excalDATAUID,this.dataComponentParent))                                    
+      window.roamAlphaAPI.moveBlock({"location":
+                                      {"parent-uid": dataComponentParentUID, 
+                                    "order": 0}, 
+                                      "block": {"uid": this.excalDATAUID}});  
+  }
+}
+
+ExcalidrawLoader.buildPage();
+const tripple_accent = String.fromCharCode(96,96,96);
+ExcalidrawLoader.updateCodeBlock(ExcalidrawLoader.sketchingUID,tripple_accent + 
+                'clojure\n' + 
+                ExcalidrawConfig.mainComponent +
+                tripple_accent);
+ExcalidrawConfig.mainComponent = null;
+
+ExcalidrawLoader.updateCodeBlock(ExcalidrawLoader.excalDATAUID,tripple_accent + 
+                'clojure\n' + 
+                ExcalidrawConfig.dataComponent +
+                tripple_accent);
+ExcalidrawConfig.dataComponent = null;
+
+delete ExcalidrawLoader;
---
+++ alpha/loader.js
@@ -0,0 +1,51 @@
+window.ExcalidrawConfig = {
+  rootPath: 'https://roam-excalidraw.com/',
+  channel: 'alpha',
+  cljCodeVersion: 'excalidraw.app.alpha.v08',
+  DEBUG : true,
+  sketchingUID : 'sketching',
+  excalDATAUID : 'ExcalDATA',
+  libs: [],
+}
+
+function getClojureNS(blockUID) {
+  const q = `[:find ?s . :where [?e :block/uid "${blockUID}"][?e :block/string ?s]]`;
+  const renderString = window.roamAlphaAPI.q(q);
+  if(renderString != null) { 
+    ptrn = /\(ns (.*)\s/g;
+    const res = ptrn.exec(renderString);
+    if(res == null) return '';
+    return res[1];
+  }
+  return '';
+}
+
+if (getClojureNS(ExcalidrawConfig.sketchingUID) != ExcalidrawConfig.cljCodeVersion) {
+  ExcalidrawConfig.libs.push (ExcalidrawConfig.rootPath + 'get.php?c='+ExcalidrawConfig.channel);
+  console.log('Excalidraw loader - need to update roam/render components');
+}
+else {
+  delete ExcalidrawConfig.sketchingUID;
+  delete ExcalidrawConfig.excalDATAUID
+}
+
+ExcalidrawConfig.libs.push (ExcalidrawConfig.rootPath+ExcalidrawConfig.channel+'/main.js');
+ExcalidrawConfig.libs.push ('https://unpkg.com/react@17/umd/react.production.min.js');
+ExcalidrawConfig.libs.push ('https://unpkg.com/react-dom@17/umd/react-dom.production.min.js');
+ExcalidrawConfig.libs.push ('https://unpkg.com/@excalidraw/excalidraw@0.4.3/dist/excalidraw.min.js');
+ExcalidrawConfig.libs.push ('https://unpkg.com/@excalidraw/utils@0.1.0-temp/dist/excalidraw-utils.min.js');
+ 
+
+ExcalidrawConfig.libs.forEach(function (x) {
+	let s = document.createElement('script');
+  s.type = "text/javascript";
+  s.src =  x;
+  s.async = false;
+  document.getElementsByTagName('head')[0].appendChild(s);  
+});
+
+delete ExcalidrawConfig.libs;
+delete ExcalidrawConfig.rootPath;
+delete ExcalidrawConfig.channel;
+delete getClojureNS;
+delete ExcalidrawConfig.cljCodeVersion;

3c666c910eb6aef49d3eb4da1606ab4b7f442124
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 18:29:26 +0100

    .

--- dev/loader.js
+++ dev/loader.js
@@ -4,14 +4,12 @@ ExcalidrawConfig.channel = 'dev';
 ExcalidrawConfig.cljCodeVersion = 'excalidraw.app.alpha.x';
 ExcalidrawConfig.DEBUG = true;
 ExcalidrawConfig.libs = [
-  ExcalidrawConfig.rootPath + 'get.php?f=main-component.cljs&c='+ExcalidrawConfig.channel,
-  ExcalidrawConfig.rootPath + 'get.php?f=data-component.cljs&c='+ExcalidrawConfig.channel,
+  ExcalidrawConfig.rootPath + 'get.php?c='+ExcalidrawConfig.channel,
+  ExcalidrawConfig.rootPath+ExcalidrawConfig.channel+'/main.js',
   'https://unpkg.com/react@17/umd/react.production.min.js',
   'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
   'https://unpkg.com/@excalidraw/excalidraw@0.4.3/dist/excalidraw.min.js',
   'https://unpkg.com/@excalidraw/utils@0.1.0-temp/dist/excalidraw-utils.min.js',
-  ExcalidrawConfig.rootPath+ExcalidrawConfig.channel+'/cljs-loader.js',
-  ExcalidrawConfig.rootPath+ExcalidrawConfig.channel+'/main.js',
 ]; 
 
 ExcalidrawConfig.libs.forEach(function (x) {

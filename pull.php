b7bccfeebe273fbf80c9cb99b2838a598b345de9
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 14:03:35 +0100

    .

--- pull.php
+++ pull.php
@@ -2,8 +2,9 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
-shell_exec( ‘cd /home/getmnvmr/roam-excalidraw.com/ && git reset –hard HEAD && git pull’ );
-}
+//if ( $_POST['payload'] ) {
+//shell_exec( ‘cd /home/getmnvmr/roam-excalidraw.com/ && git reset –hard HEAD && git pull’ );
+//}
 
+shell_exec( ‘cd /home/getmnvmr/roam-excalidraw.com/ && git pull’ );
 ?>hi

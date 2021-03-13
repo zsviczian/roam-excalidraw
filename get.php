da5fef6faea35c933629e67b91b14edd5a2e9e3d
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-03-13 18:23:44 +0100

    .

--- get.php
+++ get.php
@@ -1,18 +1,22 @@
 <?php
   $channel = ($_GET['c']);
-  $path = '/home/getmnvmr/roam-excalidraw.com/'.$channel.'/';
+  if(!empty($channel)) {
+    $path = '/home/getmnvmr/roam-excalidraw.com/'.$channel.'/';
 
-  header('content-type: application/javascript');
-  $response = 'ExcalidrawConfig.mainComponent = `';
-  $code = file_get_contents($path.'main-component.cljs');
-  $code = str_replace('\\','\\\\',$code);
-  $response = $response.$code.'`; ';
+    header('content-type: application/javascript');
+    $response = 'ExcalidrawConfig.mainComponent = `';
+    $code = file_get_contents($path.'main-component.cljs');
+    $code = str_replace('\\','\\\\',$code);
+    $response = $response.$code.'`; ';
 
-  $response = $response.'ExcalidrawConfig.dataComponent = `';
-  $code = file_get_contents($path.'data-component.cljs');
-  $code = str_replace('\\','\\\\',$code);
-  $response = $response.$code.'`; ';
-  $code = file_get_contents($path.'cljs-loader.js');
+    $response = $response.'ExcalidrawConfig.dataComponent = `';
+    $code = file_get_contents($path.'data-component.cljs');
+    $code = str_replace('\\','\\\\',$code);
+    $response = $response.$code.'`; ';
+    $code = file_get_contents($path.'cljs-loader.js');
 
-  echo $response.$code;
+    echo $response.$code;
+  } else {
+    echo 'unexpected input'
+  }
 ?>
